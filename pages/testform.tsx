f35e02a6575747b03f7ec883eed02696cc0637cf
Janmejay Joshi <publish.janmejay@gmail.com>
2023-03-20 11:45:30 +0530

    Added  Fuck Lot

--- pages/testform.tsx
+++ pages/testform.tsx
@@ -3,6 +3,7 @@ import { auth } from "@/utils/firebase";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { useRouter } from "next/router";
 import { initUser } from "@/utils/firebase/users";
+import { TextInput } from "@mantine/core";
 
 type Props = {};
 
@@ -11,10 +12,17 @@ const Test = (props: Props) => {
   const router = useRouter();
 
   useEffect(() => {
-    if (!user) router.push("/tests");
-  }, [user, router]);
+    if (!user && !loading) router.push("/tests");
+  }, [user, router, loading]);
 
-  return <div></div>;
+  return (
+    <div>
+      <TextInput placeholder="Your name" label="Full name" withAsterisk />
+      <TextInput placeholder="Your name" label="Full name" withAsterisk />
+      <TextInput placeholder="Your name" label="Full name" withAsterisk />
+      <TextInput placeholder="Your name" label="Full name" withAsterisk />
+    </div>
+  );
 };
 
 export default Test;
