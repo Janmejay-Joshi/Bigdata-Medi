25d9f08155eddf02de0b70376a5e74294f9042b5
Satvik Kaurav <satvikkaurav10@gmail.com>
2023-03-20 13:22:59 +0530

    testform

--- pages/testform.tsx
+++ pages/testform.tsx
@@ -3,8 +3,12 @@ import { auth } from "@/utils/firebase";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { useRouter } from "next/router";
 import { initUser } from "@/utils/firebase/users";
-import { Button, TextInput } from "@mantine/core";
-import { DashboardLayout } from "./layout";
+import { Button, Paper, TextInput } from "@mantine/core";
+import { DashboardLayout } from "@/layout";
+
+//forms
+import { useForm } from "@mantine/form";
+import { Box } from "tabler-icons-react";
 
 type Props = {};
 
@@ -18,20 +22,67 @@ const Testform = (props: Props) => {
   }, [user, router, loading]);
   const row = [];
 
+  const form = useForm({
+    initialValues: {
+      name: "",
+      adhar: 0,
+      diagnosis: "",
+      medicines: {
+        name: "",
+        dosage: "",
+        duration: "",
+      },
+    },
+
+    validate: {},
+  });
+
   return (
     <div>
-      <TextInput placeholder="Name" label="Full Name" withAsterisk />
-      <TextInput placeholder="adhar" label="Aadhar No." withAsterisk />
-      <TextInput placeholder="diagnosis" label="Diagnosis" withAsterisk />
+      <TextInput
+        placeholder="Name"
+        label="Full Name"
+        {...form.getInputProps("name")}
+        withAsterisk
+      />
+      <TextInput
+        placeholder="adhar"
+        label="Aadhar No."
+        {...form.getInputProps("adhar")}
+        withAsterisk
+      />
+      <TextInput
+        placeholder="diagnosis"
+        label="Diagnosis"
+        {...form.getInputProps("diagnosis")}
+        withAsterisk
+      />
       {[...Array(medVal)].map((data, index) => {
         console.log(medVal);
         return (
-          <TextInput
-            key={index}
-            placeholder="Medicines"
-            label="medicines"
-            withAsterisk
-          />
+          <Paper p={20} className="flex gap-3" key={index}>
+            <TextInput
+              // key={index}
+              placeholder="Name"
+              label="Name"
+              {...form.getInputProps("name")}
+              withAsterisk
+            />
+            <TextInput
+              // key={index}
+              placeholder="Dosage"
+              label="Dosage"
+              {...form.getInputProps("dosage")}
+              withAsterisk
+            />
+            <TextInput
+              // key={index}
+              placeholder="Duration"
+              label="Duration"
+              {...form.getInputProps("duration")}
+              withAsterisk
+            />
+          </Paper>
         );
       })}
       <Button onClick={() => setMedVal(medVal + 1)} mt={20} variant="default">
