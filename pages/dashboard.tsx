ee6d07c5f9d496c468751671f2a4fc658a937eb8
Adam-pw <adampithewan@gmail.com>
2023-03-20 13:13:19 +0530

    added cards

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,10 +1,11 @@
+import { BadgeCard } from "@/components/Card";
 import { PageContainer } from "@/components/PageContainer";
 import { PageContent } from "@/components/PageContent";
 import { CustomNextPage } from "@/interfaces/customNext";
 import { DashboardLayout } from "@/layout";
 import { auth } from "@/utils/firebase";
 
-import { Stack } from "@mantine/core";
+import { Button, Stack } from "@mantine/core";
 import { useAuthState } from "react-firebase-hooks/auth";
 
 const Dashboard: CustomNextPage = () => {
@@ -14,8 +15,79 @@ const Dashboard: CustomNextPage = () => {
     <PageContainer title="Account" fluid>
       <Stack spacing="xl">
         <PageContent title="User Details">{user?.displayName}</PageContent>
-        <PageContent title="Medical Reports">TODO</PageContent>
-        <PageContent title="Lab Reports">TODO</PageContent>
+        <PageContent title="Medical Reports">
+          <div className="flex w-full gap-2">
+            <BadgeCard
+              image={"https://avatars.githubusercontent.com/u/92779466?v=4"}
+              title={"Adam"}
+              country={"India"}
+              description={"Hello my name is adam"}
+              badges={[{ emoji: "hehe", label: "patient" }]}
+            />
+            <BadgeCard
+              image={"https://avatars.githubusercontent.com/u/92779466?v=4"}
+              title={"Adam"}
+              country={"India"}
+              description={"Hello my name is adam"}
+              badges={[{ emoji: "hehe", label: "patient" }]}
+            />
+            <BadgeCard
+              image={"https://avatars.githubusercontent.com/u/92779466?v=4"}
+              title={"Adam"}
+              country={"India"}
+              description={"Hello my name is adam"}
+              badges={[{ emoji: "hehe", label: "patient" }]}
+            />
+            <BadgeCard
+              image={"https://avatars.githubusercontent.com/u/92779466?v=4"}
+              title={"Adam"}
+              country={"India"}
+              description={"Hello my name is adam"}
+              badges={[{ emoji: "hehe", label: "patient" }]}
+            />
+          </div>
+          <div className="p-8 flex justify-center ">
+            <Button
+              variant="default"
+              className="bg-blue-500 text-white hover:text-black"
+            >
+              Show more
+            </Button>
+          </div>
+        </PageContent>
+        <PageContent title="Lab Reports">
+          <div className="flex w-full gap-2">
+            <BadgeCard
+              image={"https://avatars.githubusercontent.com/u/92779466?v=4"}
+              title={"Adam"}
+              country={"India"}
+              description={"Hello my name is adam"}
+              badges={[{ emoji: "hehe", label: "patient" }]}
+            />
+            <BadgeCard
+              image={"https://avatars.githubusercontent.com/u/92779466?v=4"}
+              title={"Adam"}
+              country={"India"}
+              description={"Hello my name is adam"}
+              badges={[{ emoji: "hehe", label: "patient" }]}
+            />
+            <BadgeCard
+              image={"https://avatars.githubusercontent.com/u/92779466?v=4"}
+              title={"Adam"}
+              country={"India"}
+              description={"Hello my name is adam"}
+              badges={[{ emoji: "hehe", label: "patient" }]}
+            />
+          </div>
+          <div className="p-8 flex justify-center ">
+            <Button
+              variant="default"
+              className="bg-blue-500 text-white hover:text-black"
+            >
+              Show more
+            </Button>
+          </div>
+        </PageContent>
       </Stack>
     </PageContainer>
   );
