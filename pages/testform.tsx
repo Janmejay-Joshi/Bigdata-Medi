6dc5823fe33abb007a138293349dbb20d495a30c
Satvik Kaurav <satvikkaurav10@gmail.com>
2023-03-20 12:42:13 +0530

    testform

--- pages/testform.tsx
+++ pages/testform.tsx
@@ -1,27 +1,44 @@
+import React, { useEffect, useState } from "react";
 import { auth } from "@/utils/firebase";
-import { TextInput } from "@mantine/core";
-import { useRouter } from "next/router";
-import { useEffect } from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
+import { useRouter } from "next/router";
+import { initUser } from "@/utils/firebase/users";
+import { Button, TextInput } from "@mantine/core";
+import { DashboardLayout } from "./layout";
 
 type Props = {};
 
-const Test = (props: Props) => {
+const Testform = (props: Props) => {
   const [user, loading, error] = useAuthState(auth);
+  const [medVal, setMedVal] = useState(1);
   const router = useRouter();
 
   useEffect(() => {
     if (!user && !loading) router.push("/tests");
   }, [user, router, loading]);
+  const row = [];
 
   return (
     <div>
-      <TextInput placeholder="Your name" label="Full name" withAsterisk />
-      <TextInput placeholder="Your name" label="Full name" withAsterisk />
-      <TextInput placeholder="Your name" label="Full name" withAsterisk />
-      <TextInput placeholder="Your name" label="Full name" withAsterisk />
+      <TextInput placeholder="Name" label="Full Name" withAsterisk />
+      <TextInput placeholder="adhar" label="Aadhar No." withAsterisk />
+      <TextInput placeholder="diagnosis" label="Diagnosis" withAsterisk />
+      {[...Array(medVal)].map((data, index) => {
+        console.log(medVal);
+        return (
+          <TextInput
+            key={index}
+            placeholder="Medicines"
+            label="medicines"
+            withAsterisk
+          />
+        );
+      })}
+      <Button onClick={() => setMedVal(medVal + 1)} mt={20} variant="default">
+        Add More Medicines
+      </Button>
     </div>
   );
 };
-
-export default Test;
+Testform.getLayout = DashboardLayout;
+export default Testform;
